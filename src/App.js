995a40e98530bd346c273924f7132aa586f0fa53
themaxopti <themaxopti@gmail.com>
2021-10-04 01:40:28 +0300

    test2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function send () {
+    return fetch('http://localhost:4000/api/someone')
+  }
+
   return (
    <div>
      Отправить запрос
+     <button onClick={send}>Send</button>
    </div>
   );
 }
