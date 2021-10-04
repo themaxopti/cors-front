cd4b8961fdd921537c91ce32008f2fa272a96fc7
themaxopti <themaxopti@gmail.com>
2021-10-04 16:15:39 +0300

    Add pay button

--- src/App.js
+++ src/App.js
@@ -10,9 +10,13 @@ function App() {
     <div>
       Отправить запрос
       <button onClick={send}>Send</button>
+
+      <a href="https://pay.fondy.eu/s/PN1WJ9UIGSX6qM" data-button="" class="f-p-b" style="--fpb-background:#dfdfdf; --fpb-color:#000000; --fpb-border-color:#000000; --fpb-border-width:1px; --fpb-font-weight:400; --fpb-font-size:16px; --fpb-border-radius:5px;">
+        <i data-text="name">Оплатить</i>
+        <i data-text="amount"> UAH</i>
+      </a>
     </div>
   )
-
 }
 
 export default App;
