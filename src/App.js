a733b6243f3ea0c3d6c7c03ee2acbf5d3bc9d0eb
themaxopti <themaxopti@gmail.com>
2021-10-04 16:18:42 +0300

    asdads

--- src/App.js
+++ src/App.js
@@ -11,10 +11,7 @@ function App() {
       Отправить запрос
       <button onClick={send}>Send</button>
 
-      <a href="https://pay.fondy.eu/s/PN1WJ9UIGSX6qM" data-button="" class="f-p-b" style="--fpb-background:#dfdfdf; --fpb-color:#000000; --fpb-border-color:#000000; --fpb-border-width:1px; --fpb-font-weight:400; --fpb-font-size:16px; --fpb-border-radius:5px;">
-        <i data-text="name">Оплатить</i>
-        <i data-text="amount"> UAH</i>
-      </a>
+     
     </div>
   )
 }
